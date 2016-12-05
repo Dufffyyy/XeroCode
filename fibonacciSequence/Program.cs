aff1fb8828e18a446a91d6da325e81b90cb6d57f
unknown <andrew.duff@XLW-5CG43810H6.inside.xero.com>
2016-12-06 10:29:08 +1100

    Initial Commit - Fib

---
+++ fibonacciSequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimeCheck;
+
+
+namespace fibonacciSequence
+{
+    class Program
+    {
+        
+        
+        
+        static void Main()
+        {
+            int fib = 0;
+            int count = 1;
+        
+
+            while (fib <= 100)
+            {
+               
+               // bool result = PrimeCheck.Program.PrimeChecker(fib);
+               //// if (result == true)
+               /// {
+                Console.WriteLine(fib);
+             //   }
+                int temp = fib;
+                fib = count;
+                count = temp + fib;
+            }
+            Console.ReadLine();
+        }
+      
+    }
+}
