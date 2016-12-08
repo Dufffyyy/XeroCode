42bcb5294ded6114bbf36293c682adf68880252d
unknown <Andrew.duff@XLW-CNU408D5KK.inside.xero.com>
2016-12-08 15:42:49 +1100

    test update

---
+++ ToDoLIstClient/ToDoLIstDBLayer/DataLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoLIstDBLayer
+{
+    public class DataLayer
+    {
+        List<string> ToDoList { get; set; }
+
+        public List<string> AddItem(string n)
+        {
+            ToDoList.Add(n);
+            return ToDoList;
+        }
+
+        public List<string> RemoveItem(string n)
+        {
+            ToDoList.Remove(n);
+            return ToDoList;
+        }
+
+        public List<string> ViewItems()
+        {
+            return ToDoList;
+        }
+
+    }
+}
---
+++ ToDoLIstClient/ToDoListBusinessLayer/BusinessLayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListBusinessLayer
+{
+    public class BusinessLayer
+    {
+        public List<string> getToDoList()
+        {
+            ToDoLIstDBLayer.DataLayer db = new ToDoLIstDBLayer.DataLayer();
+            List<string> ToDoList = db.ViewItems();
+            return ToDoList;
+        }
+
+    }
+
+}
